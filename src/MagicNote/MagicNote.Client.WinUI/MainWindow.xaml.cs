909ba7ab10d7385856b8e7f52f0f6546d9b2dbeb
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-03-12 00:32:36 +0400

    Setup the Microsoft Authentication in the WinUI app

--- src/MagicNote/MagicNote.Client.WinUI/MainWindow.xaml.cs
+++ src/MagicNote/MagicNote.Client.WinUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,30 +12,92 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace MagicNote.Client.WinUI
 {
-	/// <summary>
-	/// An empty window that can be used on its own or navigated to within a Frame.
-	/// </summary>
 	public sealed partial class MainWindow : Window
 	{
+		
+
 		public MainWindow()
 		{
 			this.InitializeComponent();
+			new MicaActivator(this);
+			ExtendsContentIntoTitleBar = true;
+			SetTitleBar(AppTitleBar);
 		}
 
-		private void myButton_Click(object sender, RoutedEventArgs e)
+		//Set the scope for API call to user.read
+		private string[] scopes = new string[] { "user.read" };
+
+		// Below are the clientId (Application Id) of your app registration and the tenant information.
+		// You have to replace:
+		// - the content of ClientID with the Application Id for your app registration
+		private const string ClientId = "ec137e7d-ceb1-453a-bb59-65dc4be40822";
+
+		private const string Tenant = "common"; // Alternatively "[Enter your tenant, as obtained from the Azure portal, e.g. kko365.onmicrosoft.com]"
+		private const string Authority = "https://login.microsoftonline.com/" + Tenant;
+
+		// The MSAL Public client app
+		private static IPublicClientApplication PublicClientApp;
+
+		private static string MSGraphURL = "https://graph.microsoft.com/v1.0/";
+		private static AuthenticationResult authResult;
+		
+
+		/// <summary>
+		/// Signs in the user and obtains an access token for Microsoft Graph
+		/// </summary>
+		/// <param name="scopes"></param>
+		/// <returns> Access Token</returns>
+		private static async Task<string> SignInUserAndGetTokenUsingMSAL(string[] scopes)
 		{
-			myButton.Content = "Clicked";
+			// Initialize the MSAL library by building a public client application
+			PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
+				.WithAuthority(Authority)
+				.WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+				 .WithLogging((level, message, containsPii) =>
+				 {
+					 Debug.WriteLine($"MSAL: {level} {message} ");
+				 }, LogLevel.Warning, enablePiiLogging: false, enableDefaultPlatformLogging: true)
+				.Build();
+
+			// It's good practice to not do work on the UI thread, so use ConfigureAwait(false) whenever possible.
+			IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
+			IAccount firstAccount = accounts.FirstOrDefault();
+
+			try
+			{
+				authResult = await PublicClientApp.AcquireTokenSilent(scopes, firstAccount)
+												  .ExecuteAsync();
+			}
+			catch (MsalUiRequiredException ex)
+			{
+				// A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenAsync to acquire a token
+				Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+				authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
+												  .ExecuteAsync()
+												  .ConfigureAwait(false);
+
+			}
+			return authResult.AccessToken;
+		}
+
+		private async void SignInButton_Click(object sender, RoutedEventArgs e)
+		{
+			await SignInUserAndGetTokenUsingMSAL(scopes);
+		}
+
+		private void SignOutButton_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
---
+++ src/MagicNote/MagicNote.Client.WinUI/MicaActivator.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using WinRT; // required to support Window.As<ICompositionSupportsSystemBackdrop>()
+using Microsoft.UI.Composition.SystemBackdrops;
+namespace MagicNote.Client.WinUI
+{
+
+	public class MicaActivator
+	{
+
+		WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See below for implementation.
+		MicaController m_backdropController;
+		SystemBackdropConfiguration m_configurationSource;
+		Window window;
+
+		public MicaActivator(Window window)
+		{
+			this.window = window;
+			TrySetSystemBackdrop();
+			window.ExtendsContentIntoTitleBar = true; 
+		}
+
+		private void Window_Activated(object sender, WindowActivatedEventArgs args)
+		{
+			m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+		}
+
+		private void Window_Closed(object sender, WindowEventArgs args)
+		{
+			// Make sure any Mica/Acrylic controller is disposed
+			// so it doesn't try to use this closed window.
+			if (m_backdropController != null)
+			{
+				m_backdropController.Dispose();
+				m_backdropController = null;
+			}
+			window.Activated -= Window_Activated;
+			m_configurationSource = null;
+		}
+
+		private void Window_ThemeChanged(FrameworkElement sender, object args)
+		{
+			if (m_configurationSource != null)
+			{
+				SetConfigurationSourceTheme();
+			}
+		}
+
+		private void SetConfigurationSourceTheme()
+		{
+			switch (((FrameworkElement)window.Content).ActualTheme)
+			{
+				case ElementTheme.Dark: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark; break;
+				case ElementTheme.Light: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
+				case ElementTheme.Default: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
+			}
+		}
+
+
+		bool TrySetSystemBackdrop()
+		{
+			if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+			{
+				m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+				m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+				// Create the policy object.
+				m_configurationSource = new SystemBackdropConfiguration();
+				window.Activated += Window_Activated;
+				window.Closed += Window_Closed;
+				((FrameworkElement)window.Content).ActualThemeChanged += Window_ThemeChanged;
+
+				// Initial configuration state.
+				m_configurationSource.IsInputActive = true;
+				SetConfigurationSourceTheme();
+
+				m_backdropController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
+				m_backdropController.TintColor = Windows.UI.Color.FromArgb(88, 88, 88, 255);
+				//m_backdropController.FallbackColor = Windows.UI.Color.FromArgb(53, 30, 40, 50);
+				//m_backdropController.TintOpacity = 0.3f;
+				//m_backdropController.LuminosityOpacity = 0.5f;
+				// Enable the system backdrop.
+				// Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+				m_backdropController.AddSystemBackdropTarget(window.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+				m_backdropController.SetSystemBackdropConfiguration(m_configurationSource);
+				return true; // succeeded
+			}
+
+			return false; // Mica is not supported on this system
+		}
+	}
+}
