d5caa14c7656a31e59ffd6ffde90fd9aeb48c123
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-03-12 06:50:44 +0400

    Setup the services and dependencies of the minimal API

--- src/MagicNote/MagicNote.Api/Program.cs
+++ src/MagicNote/MagicNote.Api/Program.cs
@@ -1,9 +1,29 @@
+using MagicNote.Core;
+using MagicNote.Core.Interfaces;
+using MagicNote.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddLanguageUnderstandingService(builder.Configuration["LanguageServiceApiKey"] ?? string.Empty);
+builder.Services.AddSingleton(sp => new HttpClient());
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton(sp =>
+{
+	var context = sp.GetRequiredService<IHttpContextAccessor>();
+	var token = context.HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer", "");
+
+	return new Microsoft.Graph.GraphServiceClient(new Microsoft.Graph.DelegateAuthenticationProvider((requestMessage) =>
+	{
+		requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+		return Task.FromResult(0);
+	}));
+});
 
 var app = builder.Build();
 
@@ -21,6 +41,11 @@ var summaries = new[]
 	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapPost("/submit-note", async ([FromBody]SubmitNoteRequest note, ILanguageUnderstnadingService languageService, Microsoft.Graph.GraphServiceClient graphClient) =>
+{
+	var result = await languageService.AnalyzeTextAsync(note.Query);
+	return Results.Ok(result);
+});
 app.MapGet("/weatherforecast", () =>
 {
 	var forecast = Enumerable.Range(1, 5).Select(index =>
