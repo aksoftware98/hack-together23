2d00241c3c333d610bff9f250f5711274ba684c5
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-03-12 22:23:00 +0400

    And the start time and end time of the tasks in the UI

---
+++ src/MagicNote/MagicNote.Client.WinUI/Converters/DateTimeToTimeSpanConverter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicNote.Client.WinUI.Converters
+{
+	public class DateTimeToTimeSpanConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			var datetime = (DateTime?)value;
+			if (datetime == null)
+				return null;
+
+			return new TimeSpan(datetime.Value.Hour, datetime.Value.Minute, datetime.Value.Second);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			var timespan = (TimeSpan)value;
+
+			return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, timespan.Hours, timespan.Minutes, timespan.Seconds);
+		}
+	}
+}
