97672f48b227c829afa3404c816fc785e7f06b8c
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-03-15 09:48:20 +0400

    Fix the contact picture binding of the display name

--- src/MagicNote/MagicNote.Client.ViewModels/Services/HttpPlanningClient.cs
+++ src/MagicNote/MagicNote.Client.ViewModels/Services/HttpPlanningClient.cs
@@ -17,7 +17,11 @@ namespace MagicNote.Client.ViewModels.Services
 	public class HttpPlanningClient : IPlanningClient
 	{
 
+		/* COMMENT THE FOLLWOING LINE AND UNCOMMENT THE AFTER TO USE THE LOCAL API IN DEBUG MODE INSTEAD OF THE ONLINE VERSION */
 		private const string BaseUrl = "https://magicnote.azurewebsites.net";
+		
+		/* UNCOMMENT THIS FOR LOCAL TESTING */
+		//private const string BaseUrl = "https://localhost:7120";
 
 		public async Task<PlanDetails> AnalyzeNoteAsync(string token, string note)
 		{
